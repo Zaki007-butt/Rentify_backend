3b3dec6528d2a04cea209c8234f6b7264a91ff91
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-06 10:59:33 -0700

    Code refactores

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import Property
 
-# class PropertySerializer(serializers.ModelSerializer):
-#   class Meta:
-#     model = Property
-#     fields = '__all__'
-
 class PropertySerializer(serializers.ModelSerializer):
   property_category_name = serializers.SerializerMethodField()
   property_type_name = serializers.SerializerMethodField()
