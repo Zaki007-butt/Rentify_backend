8743cba1f9226007413eb517331d1900ed721d7c
abdur-rehman-ali <alijoyia911@gmail.com>
2024-07-06 19:09:12 -0700

    Add search option

--- properties/views.py
+++ properties/views.py
@@ -4,15 +4,16 @@ from .models import Property, PropertyCategory, PropertyType
 from .serializers import PropertySerializer, PropertyCategorySerializer, PropertyTypeSerializer
 
 class PropertyPagination(PageNumberPagination):
-  page_size = 12
+  page_size = 24
   page_size_query_param = 'page_size'
   max_page_size = 100
 
 class PropertyViewSet(viewsets.ModelViewSet):
   queryset = Property.objects.all()
   serializer_class = PropertySerializer
-  filter_backends = [filters.OrderingFilter]
+  filter_backends = [filters.OrderingFilter, filters.SearchFilter]
   ordering_fields = ['created_at']
+  search_fields = ['title', 'description', 'address']
   pagination_class = PropertyPagination
 
   def get_queryset(self):
