7fe132fff06d1f95e26961560866818dac0850fb
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-10-25 00:22:17 +0500

    Create refresh token route

--- account/urls.py
+++ account/urls.py
@@ -8,10 +8,12 @@ from account.views import (
   UserProfileView,
   UserChangePasswordView,
 )
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
   path('register', UserRegistrationView.as_view()),
   path('login', UserLoginView.as_view()),
   path('profile', UserProfileView.as_view()),
+  path('refresh', TokenRefreshView.as_view()),
   path('change-password', UserChangePasswordView.as_view()),
 ]
